f725218e8a7464500f60de99809ed0dcf8729559
Paul SaundersPaul Saunders <psaunder@ualberta.ca>
2019-02-27 08:53:44 -0700

    removed the comments for showing the players' moves

--- main.py
+++ main.py
@@ -24,8 +24,6 @@ def main():
         while gameOn:
             playerMoves = reversi.findValidMoves(playerColour)
             botMoves = reversi.findValidMoves(botColour)
-            # print("Player's moves:", playerMoves)
-            # print("Bot's moves:", botMoves)
             if reversi.isGameOver(playerMoves) or reversi.isGameOver(botMoves):
                 gameOn = False
             else:
