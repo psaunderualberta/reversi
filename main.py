3a4be9ac2a785c52d36695522c91789af7000370
Paul SaundersPaul Saunders <psaunder@ualberta.ca>
2019-02-28 17:34:39 -0700

    Changed the format of reversi.py so that the format of main is cleaner, and so it abides by the endgame conditions

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from reversi import Reversi
 
+'''
+Questions to ask Tuero:
+1. It is okay to have extra methods in the program?
+2. Is the inventwithpython citation okay?
+3. Do I have to include all parameters? (setPlayerColour)
+4. I need help with findValidMoves
+'''
 
 
 def main():
@@ -31,20 +38,20 @@ def main():
         playersTurn = validMove
 
         while gameOn:
-            reversi.findValidMoves(playerColour)
-            reversi.findValidMoves(botColour, True)
-            if reversi.isGameOver():
-                gameOn = False
-            else:
-                # Don't display the board again if the
-                # player's input was invalid
-                if validMove and playersTurn:
-                    reversi.displayBoard()
-                try:
-                    if playersTurn:
+            # reversi.findValidMoves(playerColour)
+            # reversi.findValidMoves(botColour, True)
+            # Don't display the board again if the
+            # player's input was invalid
+            if validMove and playersTurn:
+                reversi.displayBoard()
+            try:
+                if playersTurn:
+                    if reversi.findValidMoves(playerColour) == []:
+                        gameOn = False
+                    else:
                         row = input("Which row you would you like to play? ")
                         if row.lower() == 'quit': # easy way to stop the game at any time
-                            reversi.isGameOver(quit=True)
+                            print("The game was stopped by the player.")
                             gameOn = False
                         else:
                             checkInput(row, reversi)
@@ -52,24 +59,28 @@ def main():
                             checkInput(col, reversi)
                             playerPosition = (int(row), int(col))
                             reversi.isPositionValid(playerPosition, playerColour)
-                except AssertionError as e:
-                    validMove = False
-                    print(e.args[0])
-                except Exception:
-                    print("Error: Invalid input. Please enter an integer from 0 to 7.")
-                else:
-                    validMove = True
-                    if gameOn:
-                        if playersTurn:
-                            reversi.makeMovePlayer(playerPosition)
-                            playersTurn = False
+            except AssertionError as e:
+                validMove = False
+                print(e.args[0])
+            except Exception as e:
+                validMove = False
+                print("Error: Invalid input. Please enter an integer from 0 to 7.")
+            else:
+                validMove = True
+                if gameOn:
+                    if playersTurn:
+                        reversi.makeMovePlayer(playerPosition)
+                        playersTurn = False
+                    else:
+                        if smartBot:
+                            botPosition = reversi.makeMoveSmart()
                         else:
-                            if smartBot:
-                                botPosition = reversi.makeMoveSmart()
-                            else:
-                                botPosition = reversi.makeMoveNaive()
+                            botPosition = reversi.makeMoveNaive()
+                        if botPosition == False:
+                            gameOn = False
+                        else:
                             reversi.makeMovePlayer(botPosition, bot=True)
-                            playersTurn = True
+                        playersTurn = True
             reversi.getScore(playerColour)
             reversi.getScore(botColour)
 
@@ -82,14 +93,14 @@ def main():
             continueGame = False
 
 
-def checkInput(dig, reversi):
+def checkInput(num, reversi):
         # Checks the player's input to see if it is valid.
         # The naive bot's input is checked in reversi's 
         # 'botValidation' method, whereas the smart bot does not need
         # input validation as it picks one from the set of valid moves
         try:
-            int(dig)
-            assert reversi.inBoard(int(dig)), "Please choose an integer between 0 and 7."
+            int(num)
+            assert reversi.inBoard(int(num)), "Please choose an integer between 0 and 7."
         except Exception:
             raise
 
